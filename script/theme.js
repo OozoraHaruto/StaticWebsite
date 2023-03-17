823fcd2b37865e82ddcaffc703b2052e97492097
春音 <chibi.taiyoozora@gmail.com>
2023-03-17 23:08:12 +0800

    FIX: remove showActiveTheme as it will cause and error

--- script/theme.js
+++ script/theme.js
@@ -21,18 +21,18 @@
 
   setTheme(getPreferredTheme())
 
-  const showActiveTheme = theme => {
-    const activeThemeIcon = document.querySelector('.theme-icon-active use')
-    const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
-    const svgOfActiveBtn = btnToActive.querySelector('svg use').getAttribute('href')
+  // const showActiveTheme = theme => {
+  //   const activeThemeIcon = document.querySelector('.theme-icon-active use')
+  //   const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
+  //   const svgOfActiveBtn = btnToActive.querySelector('svg use').getAttribute('href')
 
-    document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
-      element.classList.remove('active')
-    })
+  //   document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
+  //     element.classList.remove('active')
+  //   })
 
-    btnToActive.classList.add('active')
-    activeThemeIcon.setAttribute('href', svgOfActiveBtn)
-  }
+  //   btnToActive.classList.add('active')
+  //   activeThemeIcon.setAttribute('href', svgOfActiveBtn)
+  // }
 
   window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
     if (storedTheme !== 'light' || storedTheme !== 'dark') {
@@ -41,7 +41,7 @@
   })
 
   window.addEventListener('DOMContentLoaded', () => {
-    showActiveTheme(getPreferredTheme())
+    // showActiveTheme(getPreferredTheme())
 
     document.querySelectorAll('[data-bs-theme-value]')
       .forEach(toggle => {
@@ -49,7 +49,7 @@
           const theme = toggle.getAttribute('data-bs-theme-value')
           localStorage.setItem('theme', theme)
           setTheme(theme)
-          showActiveTheme(theme)
+          // showActiveTheme(theme)
         })
       })
   })
